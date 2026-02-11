6844b4afa7f2d69962352c5a6797bfc6cbca4fa2
natthapon3090 <garfield11kp@gmail.com>
2026-02-11 17:10:47 +0700

    Update test_funny_string.py

--- tests/test_funny_string.py
+++ tests/test_funny_string.py
@@ -6,3 +6,15 @@ class TestFunnyString(unittest.TestCase):
         self.assertEqual(funny_string("acxz"), "Funny")
   def test_not_funny(self):
         self.assertEqual(funny_string("bcxz"), "Not Funny")
+
+  def test_single_character(self):
+          self.assertEqual(funny_string("a"), "Funny")
+    
+  def test_two_characters(self):
+          self.assertEqual(funny_string("aa"), "Funny")
+    
+  def test_repeated_characters(self):
+        self.assertEqual(funny_string("aaa"), "Funny")
+
+  def test_long_not_funny(self):
+        self.assertEqual(funny_string("abcdba"), "Not Funny")
