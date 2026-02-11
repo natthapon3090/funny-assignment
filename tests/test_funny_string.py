4a823b4c089e96ddda6cb34c6676081a779c836e
natthapon3090 <garfield11kp@gmail.com>
2026-02-11 17:08:57 +0700

    Update test_funny_string.py

--- tests/test_funny_string.py
+++ tests/test_funny_string.py
@@ -2,3 +2,7 @@ import unittest
 from funny_string import funny_string
 
 class TestFunnyString(unittest.TestCase):
+  def test_funny(self):
+        self.assertEqual(funny_string("acxz"), "Funny")
+  def test_not_funny(self):
+        self.assertEqual(funny_string("bcxz"), "Not Funny")
